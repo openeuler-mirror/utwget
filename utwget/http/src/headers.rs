fd0d99c461416577114ff2c26f7b543742edffdf
hailianguniontech <mahailiang@uniontech.com>
2026-08-05 15:15:03 +0800

    [http] add const `CONTENT_LENGTH`
    Add const `CONTENT_LENGTH` to `headers.rs` in `http` crate.

---
+++ utwget/http/src/headers.rs
@@ -0,0 +1,7 @@
+//! HTTP header constants and parsing utilities.
+//!
+//! This module provides constants for common HTTP header names and functions
+//! for parsing and formatting header lines.
+
+/// Content-Length header - indicates the size of the message body in bytes.
+pub const CONTENT_LENGTH: &str = "Content-Length";
