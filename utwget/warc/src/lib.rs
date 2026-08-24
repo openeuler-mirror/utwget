cb64c7fa82ee59e9d6334a605ad951ebb4080ba2
hailianguniontech <mahailiang@uniontech.com>
2026-08-24 15:05:43 +0800

    [warc] add functions `format_date + format_record_id`
    Add functions `format_date + format_record_id` to `lib.rs` in `warc` crate.

--- utwget/warc/src/lib.rs
+++ utwget/warc/src/lib.rs
@@ -81,3 +81,11 @@ pub trait WarcWriter: Send {
 
     fn close(&mut self) -> Result<()>;
 }
+
+pub fn format_date(dt: DateTime<Utc>) -> String {
+    dt.format("%Y-%m-%dT%H:%M:%SZ").to_string()
+}
+
+pub fn format_record_id() -> String {
+    format!("<urn:uuid:{}>", generate_uuid_v4())
+}
